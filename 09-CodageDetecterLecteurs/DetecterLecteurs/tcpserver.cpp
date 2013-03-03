a1f424941463cac11371147c17a2f5ba146fee4c
BriceJamin <brice.jamin@hotmail.fr>
2013-03-04 00:41:23 +0100

    [Com] Inutile

--- 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserver.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserver.cpp
@@ -3,9 +3,6 @@
 TcpServer::TcpServer(QObject *parent) :
     QTcpServer(parent)
 {
-    /* Le renseignement de l'adresse et du port
-        se fera lors de l'appel de listen().
-    */
 }
 
 void TcpServer::incomingConnection(int socketDescriptor)
