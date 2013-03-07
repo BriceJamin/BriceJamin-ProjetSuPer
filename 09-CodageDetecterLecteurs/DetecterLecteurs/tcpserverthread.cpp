7d388e22beef8d489ff383576d324fe866efe4f5
BriceJamin <brice.jamin@hotmail.fr>
2013-03-07 16:26:48 +0100

    [New] Connexion à la BDD

--- 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
@@ -1,5 +1,6 @@
 #include "tcpserverthread.h"
 #include <QHostAddress>
+#include <QSqlDatabase>
 
 TcpServerThread::TcpServerThread(int socketDescriptor, QObject *parent)
     : QThread(parent), socketDescriptor(socketDescriptor)
@@ -22,6 +23,15 @@ void TcpServerThread::run()
     QHostAddress addressQHost = tcpSocket.peerAddress();
 
     /* TODO : Demander à la BDD si c'est un lecteur. */
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("bdd_super");
+    db.setUserName("user_super");
+    db.setPassword("mdp_super");
+    if (!db.open())
+        qDebug() << "Impossible d'établir une connexion avec la Bdd.";
+    else
+        qDebug() << "Connexion à la bdd réussie.";
 
     /* TODO : Si c'est non, se déconnecter */
 
