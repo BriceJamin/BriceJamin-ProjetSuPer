d10e4b52a024497f1b39b5897fede10492a8d3a7
BriceJamin <brice.jamin@hotmail.fr>
2013-03-07 18:50:44 +0100

    [New] Identification d'un lecteur

--- 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
@@ -20,7 +20,7 @@ void TcpServerThread::run()
      }
 
     /* Récupère l'adresse du client. */
-    QHostAddress addressQHost = tcpSocket.peerAddress();
+    QString peerAddress = tcpSocket.peerAddress().toString();
 
     /* Demande à la BDD si c'est un lecteur. */
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
@@ -29,16 +29,43 @@ void TcpServerThread::run()
     db.setUserName("user_super");
     db.setPassword("mdp_super");
     if (!db.open())
-        qDebug() << "Impossible d'établir une connexion avec la Bdd.";
+    {
+        qDebug() << "Impossible d'etablir une connexion avec la Bdd.";
+
+        /* TODO : Sortir du thread */
+    }
     else
-        qDebug() << "Connexion à la bdd réussie.";
+        qDebug() << "Connexion a la bdd reussie.";
 
+    QSqlQuery query(db);
+    query.exec("SELECT  num_lecteur, num_lieu, ip, estConnecte FROM lecteur WHERE ip like \"" + peerAddress + "\"");
+    if(!query.isActive())
+    {
+        qDebug() << "Requete (Ce lecteur d'ip X existe t'il ?) erronnée.";
 
+        /* TODO : Tuer le thread */
+    }
+    else if(query.size() == 0)
+    {
+        qDebug() << "Le client n'est pas un lecteur.";
 
-    /* TODO : Si c'est non, se déconnecter */
+        /* TODO : Emettre un signal "intrus detecte" */
+        /* TODO : Déconnecter le client */
+        /* TODO : Tuer le thread */
+    }
+    else
+    {
+        query.next();
+        qDebug() << "Le client est le lecteur de numero " + QString::number(query.value(0).toInt()) + ",";
+        qDebug() << "de lieu numero " + QString::number(query.value(1).toInt()) + ",";
+        qDebug() << "d'ip " + query.value(2).toString() + ",";
+        qDebug() << "et de estConnecte " + QString::number(query.value(3).toInt()) + ".";
 
-    /* TODO : Si c'est oui, lire les trames reçues
-        tant que le lecteur reste connecté. */
+        /* TODO : Créer une classe Lecteur */
+        /* TODO : Instancier un objet de la classe Lecteur */
+        /* TODO : Emettre un signal "lecteur connecté" avec ce lecteur en paramètre */
 
-    /* TODO : Se déconnecter. */
+        /* TODO : Lire les trames reçues tant que le lecteur reste connecté. */
+        /* TODO : Tuer le thread */
+    }
 }
