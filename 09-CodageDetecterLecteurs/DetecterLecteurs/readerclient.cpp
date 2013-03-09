68d884636b7ce60ccf50a15928100be71724c7c6
BriceJamin <brice.jamin@hotmail.fr>
2013-03-09 21:05:48 +0100

    Revert "Sauvegarde avant de partir"
    This reverts commit e9f2dae5bcdb1ca1bf691cb3126472dd28570700.

--- 09-CodageDetecterLecteurs/DetecterLecteurs/readerclient.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/readerclient.cpp
@@ -1,11 +1,6 @@
 #include "readerclient.h"
 
-ReaderClient::ReaderClient
-    (int num, int lieu, QString ip, bool estConnecte, QObject *parent = 0)
-    : QObject(parent),
-      number(num),
-      placeId(lieu),
-      address(ip),
-      isConnected(estConnecte)
+ReaderClient::ReaderClient(QObject *parent) :
+    QObject(parent)
 {
 }
--- 09-CodageDetecterLecteurs/DetecterLecteurs/readerclient.h
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/readerclient.h
@@ -6,19 +6,12 @@
 class ReaderClient : public QObject
 {
     Q_OBJECT
-
+public:
+    explicit ReaderClient(QObject *parent = 0);
+    
 signals:
 public slots:
-
-public:
-    explicit ReaderClient(Reader reader, QObject *parent = 0);
-    int number() const;
-    int placeId() const;
-    QString address() const;
-    bool isConnected() const;
-
-private:
-    Reader reader;
+    
 };
 
 #endif // READERCLIENT_H
