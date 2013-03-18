62a27503389fb7829b64d4d2c71ad3c79e92000a
BriceJamin <brice.jamin@hotmail.fr>
2013-03-18 01:23:06 +0100

    [Fix] Connexion BDD reste ouverte

--- 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/tcpserverthread.cpp
@@ -38,49 +38,58 @@ void TcpServerThread::run()
 
     /* Demande à la BDD si c'est un lecteur. */
     /* TODO : Créer une classe servant d'interface à la BDD */
-    /* TODO : Se renseigner sur l'accès à une BDD via des threads */
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL", QString::number(QThread::currentThreadId()));
-    db.setHostName("localhost");
-    db.setDatabaseName("bdd_super");
-    db.setUserName("user_super");
-    db.setPassword("mdp_super");
-    if (!db.open())
+    QString nameDatabaseConnexion = QString::number(QThread::currentThreadId());
+    unsigned int number;
+    unsigned int placeId;
+    QString address;
+    bool isConnected;
     {
-        qFatal("Impossible d'etablir une connexion avec la Bdd.");
-        /* Termine l'exécution du thread */
-        return;
+        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL", nameDatabaseConnexion);
+        db.setHostName("localhost");
+        db.setDatabaseName("bdd_super");
+        db.setUserName("user_super");
+        db.setPassword("mdp_super");
+        if (!db.open())
+        {
+            qFatal("Impossible d'etablir une connexion avec la Bdd.");
+            /* Termine l'exécution du thread */
+            return;
+        }
+
+        qDebug() << "TcpServerThread : Connexion a la bdd reussie.";
+
+        QSqlQuery query(db);
+        query.exec("SELECT  num_lecteur, num_lieu, ip, estConnecte FROM lecteur WHERE ip like \"" + clientAddress + "\"");
+        if(!query.isActive())
+        {
+            qFatal("TcpServerThread : Requete sql (Ce lecteur d'ip X existe t'il ?) erronnée.");
+
+            /*  Termine l'exécution du thread */
+            return;
+        }
+
+        if(query.size() == 0)
+        {
+            qDebug() << "TcpServerThread : Le client " + clientAddress + " n'est pas un lecteur.";
+
+            /* Signale la détection d'un intrus */
+            emit sig_intruderDetected(clientAddress);
+
+            /* TODO : Déconnecter le client proprement */
+
+            /* Termine l'exécution du thread */
+            return;
+        }
+
+        query.next();
+        number = query.value(0).toInt();
+        placeId = query.value(1).toInt();
+        address = query.value(2).toString();
+        isConnected = query.value(3).toBool();
+        db.close();
     }
 
-    qDebug() << "TcpServerThread : Connexion a la bdd reussie.";
-
-    QSqlQuery query(db);
-    query.exec("SELECT  num_lecteur, num_lieu, ip, estConnecte FROM lecteur WHERE ip like \"" + clientAddress + "\"");
-    if(!query.isActive())
-    {
-        qFatal("TcpServerThread : Requete sql (Ce lecteur d'ip X existe t'il ?) erronnée.");
-
-        /*  Termine l'exécution du thread */
-        return;
-    }
-
-    if(query.size() == 0)
-    {
-        qDebug() << "TcpServerThread : Le client " + clientAddress + " n'est pas un lecteur.";
-
-        /* Signale la détection d'un intrus */
-        emit sig_intruderDetected(clientAddress);
-
-        /* TODO : Déconnecter le client proprement */
-
-        /* Termine l'exécution du thread */
-        return;
-    }
-
-    query.next();
-    unsigned int number = query.value(0).toInt();
-    unsigned int placeId = query.value(1).toInt();
-    QString address = query.value(2).toString();
-    bool isConnected = query.value(3).toBool();
+    QSqlDatabase::removeDatabase(nameDatabaseConnexion);
 
     ReaderData readerData(number, placeId, address, isConnected);
     Reader reader(readerData);
