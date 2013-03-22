5a4f25fe4890925ac65baf12f0883e4c590e4986
BriceJamin <brice.jamin@hotmail.fr>
2013-03-23 00:46:40 +0100

    [Com] TODOs de la méthode manage()

--- 09-CodageDetecterLecteurs/DetecterLecteurs/tcpclientmanager.cpp
+++ 09-CodageDetecterLecteurs/DetecterLecteurs/tcpclientmanager.cpp
@@ -8,4 +8,7 @@ TcpClientManager::TcpClientManager(int socketDescriptor, QTcpServer *parent) :
 void TcpClientManager::manage()
 // Slot appelé par started() lors de l'exécution d'un nouveau thread
 {
+    // TODO : Récupérer le QTcpSocket du client à l'aide de son descripteur
+    // TODO : Signaler intruderDetected si ce n'est pas un reader puis quitter
+    // TODO : Signaler readerDetected si c'est un reader
 }
