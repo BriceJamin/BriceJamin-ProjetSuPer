db213a58ea70d396f6fe1db26f1d74b41c32969d
BriceJamin <brice.jamin@hotmail.fr>
2013-03-28 17:06:00 +0100

    [New] IHM accède à Server

--- 09-CodageDetecterLecteurs/MonoThread/mainwindow.cpp
+++ 09-CodageDetecterLecteurs/MonoThread/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(Server* server, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    _server = server;
+    ui->addressLineEdit->setText(server->address());
+    ui->portSpinBox->setValue(server->port());
 }
 
 MainWindow::~MainWindow()
--- 09-CodageDetecterLecteurs/MonoThread/mainwindow.h
+++ 09-CodageDetecterLecteurs/MonoThread/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "server.h"
 
 namespace Ui {
 class MainWindow;
@@ -12,11 +13,12 @@ class MainWindow : public QMainWindow
     Q_OBJECT
     
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(Server* server, QWidget *parent = 0);
     ~MainWindow();
     
 private:
     Ui::MainWindow *ui;
+    Server* _server;
 };
 
 #endif // MAINWINDOW_H
