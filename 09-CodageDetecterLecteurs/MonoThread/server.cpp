6fed7fea4c4e808dc6ab4035021e12a7603ff7f5
BriceJamin <brice.jamin@hotmail.fr>
2013-03-28 16:32:44 +0100

    [Fix] Implementation vide des slots du Server

--- 09-CodageDetecterLecteurs/MonoThread/server.cpp
+++ 09-CodageDetecterLecteurs/MonoThread/server.cpp
@@ -4,3 +4,13 @@ Server::Server(QString address, unsigned int port, QObject *parent) :
     QTcpServer(parent), _address(address), _port(port)
 {
 }
+
+void Server::slot_switchOn()
+{
+
+}
+
+void Server::slot_switchOff()
+{
+
+}
