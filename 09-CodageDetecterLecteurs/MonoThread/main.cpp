89a1da661d2bc54b252bc265a7b09c23fa046694
BriceJamin <brice.jamin@hotmail.fr>
2013-04-01 17:57:53 +0200

    [Fix] N'afficher qu'une window

--- 09-CodageDetecterLecteurs/MonoThread/main.cpp
+++ 09-CodageDetecterLecteurs/MonoThread/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char *argv[])
 
     Server server("127.0.0.254", "43210");
     MainWindow w1(&server);
-    MainWindow w2(&server);
+    //MainWindow w2(&server);
     w1.show();
-    w2.show();
+    //w2.show();
     
     return a.exec();
 }
