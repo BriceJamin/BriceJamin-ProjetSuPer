f44ffe49f5ac8928012798b2f3f681fa0db9d0e4
BriceJamin <brice.jamin@hotmail.fr>
2013-04-01 16:42:36 +0200

    [Fix] Ajout qDebug dans constructeur

--- 09-CodageDetecterLecteurs/MonoThread/clientmanager.cpp
+++ 09-CodageDetecterLecteurs/MonoThread/clientmanager.cpp
@@ -1,6 +1,8 @@
 #include "clientmanager.h"
+#include <QDebug>
 
 ClientManager::ClientManager(int socketDescriptor) :
     QObject(), _socketDescriptor(socketDescriptor)
 {
+    qDebug() << Q_FUNC_INFO << socketDescriptor;
 }
