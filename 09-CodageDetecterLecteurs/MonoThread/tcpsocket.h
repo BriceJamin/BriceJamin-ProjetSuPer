26ac5aa75da380765793ae73c995049e77151105
BriceJamin <brice.jamin@hotmail.fr>
2013-04-01 20:36:06 +0200

    [New] Suivi des signaux de QTcpServer dans TcpServer

--- 09-CodageDetecterLecteurs/MonoThread/tcpsocket.h
+++ 09-CodageDetecterLecteurs/MonoThread/tcpsocket.h
@@ -8,6 +8,18 @@ class TcpSocket : public QTcpSocket
 Q_OBJECT
 public:
     explicit TcpSocket(QObject *parent = 0);
+
+private slots:
+    void slot_connected();
+    void slot_disconnected();
+    void slot_error(QAbstractSocket::SocketError);
+    void slot_hostFound();
+    void slot_proxyAuthenticationRequired();
+    void slot_stateChanged(QAbstractSocket::SocketState);
+    void slot_aboutToClose();
+    void slot_readyRead();
+    void slot_readChannelFinished();
+    void slot_bytesWritten(qint64);
 };
 
 #endif // TCPSOCKET_H
