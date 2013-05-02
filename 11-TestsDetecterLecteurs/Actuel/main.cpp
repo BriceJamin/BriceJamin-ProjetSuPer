c0caa3b03a3ebbef1b5400bde82016cd83fed1fd
Brice Jamin <brice.jamin@hotmail.fr>
2013-05-02 18:30:30 +0200

    11. [Fix] Adresse et port du serveur
    Utilisation des valeurs donnés sur le site afin de se mettre en
    conditions proches du réel.

--- 11-TestsDetecterLecteurs/Actuel/main.cpp
+++ 11-TestsDetecterLecteurs/Actuel/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     qDebug();
 
-    Server server("127.0.0.254", "43210");
+    Server server("192.168.60.100", "2222");
     MainWindow w1(&server);
     //MainWindow w2(&server);
     w1.show();
